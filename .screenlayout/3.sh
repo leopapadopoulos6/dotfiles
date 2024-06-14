21570448a7720941c21494936c13c3f75f82d848
leo <leopapadopoulos6@gmail.com>
2024-06-13 22:22:24 -0400

    dots-games

---
+++ .screenlayout/3.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DVI-D-0 --off --output HDMI-0 --mode 1920x1080 --pos 889x0 --rotate normal --output DP-0 --mode 1920x1080 --pos 1920x1080 --rotate normal --output DP-1 --off --output DP-2 --mode 1920x1080 --pos 0x1080 --rotate normal --output DP-3 --off --output DP-4 --off --output DP-5 --off --output DP-1-4 --off --output HDMI-1-2 --off --output HDMI-1-3 --off
